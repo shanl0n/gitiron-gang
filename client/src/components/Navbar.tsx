192c054ed69aece2b0708c3e3d94598193c18872
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-10-16 15:08:42 +0100

    updated styling on Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper=styled.nav`
+  background-color: white;
+  display: flex;
+`
+const Options=styled.div`
+  color: black;
+`
 
 const Navbar = () => {
   return (
-  <>
-    <div>League</div>
-    <div>My Team</div>
-    <div>Players</div>
-  </>)
+  <Wrapper>
+    <Options>League</Options>
+    <Options>My Team</Options>
+    <Options>Players</Options>
+  </Wrapper>)
 };
 
 export default Navbar;
