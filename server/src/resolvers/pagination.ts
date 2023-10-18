3dd754dbb761854990c2214d9e77fd46419070b3
Sean Hanlon <137118620+shanl0n@users.noreply.github.com>
2023-10-18 14:40:40 +0100

    pagination with page numbers
    * Added two possible solutions to pagination
    
    * make page numbers work

--- server/src/resolvers/pagination.ts
+++ server/src/resolvers/pagination.ts
@@ -1,63 +1,43 @@
 import { ApolloError } from "apollo-server-express";
-import { Connection, PaginationInput, Node, Edge } from "../schema/types";
+import { Connection, PaginationInput, Node } from "../schema/types";
 import { Filter, WithId } from "mongodb";
 import { Collection } from "mongoose";
-import MongoPaging from "mongo-cursor-pagination";
-import { encode as cursorEncode } from "mongo-cursor-pagination/src/utils/bsonUrlEncoding";
 
 const validatePaginationInput = (input: PaginationInput) => {
-  if (input.first && input.last) {
-    throw new ApolloError("Cannot give both first and last args", "BAD_USER_INPUT");
+  if (input.page <= 0 || input.pageSize <= 0) {
+    throw new ApolloError(
+      "The args page and pageSize must be a positive int",
+      "BAD_USER_INPUT"
+    );
   }
+};
 
-  if (input.beforeCursor && input.afterCursor) {
-    throw new ApolloError("Cannot give both beforeCursor and afterCursor args", "BAD_USER_INPUT");
-  }
-
-  if (input.first <= 0 || input.last <= 0) {
-    throw new ApolloError("The args first and last must be a positive int", "BAD_USER_INPUT");
-  }
-
-  if (input.beforeCursor === "" || input.afterCursor === "") {
-    throw new ApolloError("The args beforeCursor and afterCursor cannot be empty strings", "BAD_USER_INPUT");
-  }
-}
-
-interface MongoPage<T extends Node> {
-  results: WithId<T>[],
-  hasNext: boolean,
-  hasPrevious: boolean,
-  next?: string,
-  previous?: string;
-}
-
-export const paginationQuery = async <T extends Node>(collection: Collection<T>, input?: PaginationInput, query?: Filter<T>): Promise<Connection<T>> => {
+export const paginationQuery = async <T extends Node>(
+  collection: Collection<T>,
+  input?: PaginationInput,
+  filter?: Filter<T>
+): Promise<Connection<T>> => {
   if (input) validatePaginationInput(input);
 
-  let limit = 25;
+  const itemCount = await collection.countDocuments(filter);
+  const pageSize = input?.pageSize || 25;
+  const pageCount = Math.ceil(itemCount / pageSize);
 
-  if (input?.first) limit = input.first;
-  if (input?.last) limit = input.last;
+  const currentPage = input?.page || 1;
+  const offset = (currentPage - 1) * pageSize;
 
-  const page = await MongoPaging.find(collection, {
-    limit,
-    query,
-    next: input?.afterCursor,
-    previous: input?.beforeCursor,
-  }) as MongoPage<T>;
-
-  const edges = page.results.map((item) => ({
-    cursor: cursorEncode(item._id),
-    node: item,
-  } as Edge<T>));
+  const page = await collection
+    .find(filter)
+    .limit(pageSize)
+    .skip(offset)
+    .toArray();
 
   return {
     pageInfo: {
-      hasNextPage: page.hasNext as boolean,
-      hasPreviousPage: page.hasPrevious as boolean,
-      startCursor: page.previous as string,
-      endCursor: page.next as string,
+      pageCount,
+      currentPage,
+      itemCount,
     },
-    edges,
+    nodes: page as T[],
   };
-}
+};
--- server/src/schema/types.ts
+++ server/src/schema/types.ts
@@ -1,10 +1,8 @@
 import { FantasyTeamModel, PlayerModel } from "../datasource/models";
 
 export interface PaginationInput {
-  first: number;
-  last: number;
-  afterCursor: string;
-  beforeCursor: string;
+  page?: number;
+  pageSize?: number;
 }
 
 export type PlayersInput = PaginationInput;
@@ -41,32 +39,22 @@ export interface GameStatsSummary{
 }
 
 interface PageInfo {
-  hasNextPage: boolean;
-  hasPreviousPage: boolean;
-  startCursor?: string;
-  endCursor?: string;
+  pageCount: number;
+  currentPage: number;
+  itemCount: number;
 }
 
 export interface Node {
   id: string;
 }
 
-export interface Edge<T extends Node> {
-  cursor: string;
-  node: T;
-}
-
 export interface Connection<T extends Node> {
   pageInfo: PageInfo;
-  edges: Edge<T>[];
+  nodes: T[];
 }
 
 export interface PlayerConnection extends Connection<Player> {
-  edges: PlayerEdge[];
-}
-
-interface PlayerEdge extends Edge<Player> {
-  node: Player;
+  nodes: Player[];
 }
 
 export type Player = Omit<PlayerModel, "team_id"> & {
